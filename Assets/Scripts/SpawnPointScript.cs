9888964476832ca239f8b906f3898cbeefcb4ac0
Lucas Dewil <lucas.dewil@student.howest.be>
2024-01-09 20:37:33 +0100

    First area spawning test, Need to test at the campus

---
+++ Assets/Scripts/SpawnPointScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPointScript : MonoBehaviour
+{
+    public double minLatitude;
+    public double maxLatitude;
+    public double minLongitude;
+    public double maxLongitude;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/TextChanger.cs
+++ Assets/Scripts/TextChanger.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class TextChanger : MonoBehaviour
 {
+    [System.Serializable]
+    public class LocationData
+    {
+        public float latitude;
+        public float longitude;
+        public float accuracy;
+    }
 
+
+
+    public GameObject player;
+    public double minLatitude;
+    public double maxLatitude;
+    public double minLongitude;
+    public double maxLongitude;
     // Reference to the UI Text component
     public TMPro.TextMeshProUGUI textComponent;
     private string previous;
+    public Transform spawnPoint;
     void FixedUpdate()
     {
         // Call the JS method on update, if needed
@@ -20,9 +35,26 @@ public class TextChanger : MonoBehaviour
         {
             // Convert the pointer to a string
             string location = Marshal.PtrToStringAnsi((IntPtr)ptr);
-            Debug.Log("Location: " + location);
+            Debug.Log(location);
             // Here you can update your UI or do other operations with the location data
             textComponent.text = location;
+            // if the location is between the min and max latitudes and longitudes go to spawn point
+            // Parse the JSON string into the LocationData class
+            LocationData locationData = JsonUtility.FromJson<LocationData>(location);
+            Debug.Log("Latitude: " + locationData.latitude);
+            Debug.Log("Longitude: " + locationData.longitude);
+            Debug.Log("Accuracy: " + locationData.accuracy);
+            Debug.Log("Min Latitude: " + minLatitude);
+            Debug.Log("Max Latitude: " + maxLatitude);
+            Debug.Log("Min Longitude: " + minLongitude);
+            Debug.Log("Max Longitude: " + maxLongitude);
+            if (locationData.latitude > minLatitude && locationData.latitude < maxLatitude &&
+                locationData.longitude > minLongitude && locationData.longitude < maxLongitude)
+            {
+                Debug.Log("In bounds");
+                player.transform.position = spawnPoint.position;
+            }
+
         }
         else
         {
